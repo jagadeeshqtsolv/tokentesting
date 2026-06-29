d886c10f2d1b1e671c96854e296ad8a45110ef89
jagadeesh <jagadeesh@qtsolv.com>
2026-06-29 23:25:57 +0530

    chore: sync test framework [2026-06-29T17:55:57.435Z]

--- support/fixtures.ts
+++ support/fixtures.ts
@@ -1,6 +1,5 @@
 import { test as base, expect } from "@playwright/test";
 import { AuditLogPage } from "../pageobjects/AuditLogPage";
-import { CommonPage } from "../pageobjects/CommonPage";
 import { LoginPage } from "../pageobjects/LoginPage";
 import { OverviewPage } from "../pageobjects/OverviewPage";
 import { RolesAndAccessPage } from "../pageobjects/RolesAndAccessPage";
@@ -8,7 +7,6 @@ import { UserManagementPage } from "../pageobjects/UserManagementPage";
 
 type AppFixtures = {
   auditLogPage: AuditLogPage;
-  commonPage: CommonPage;
   loginPage: LoginPage;
   overviewPage: OverviewPage;
   rolesAndAccessPage: RolesAndAccessPage;
@@ -18,9 +16,6 @@ type AppFixtures = {
 export const test = base.extend<AppFixtures>({
   auditLogPage: async ({ page }, use) => {
     await use(new AuditLogPage(page));
-  },
-  commonPage: async ({ page }, use) => {
-    await use(new CommonPage(page));
   },
   loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
